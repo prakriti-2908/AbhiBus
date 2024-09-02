7c212ef38a3b619add139d6b1b0fe8c2f53e6371
PrakritiJha <prakritijha29aug@gmail.com>
2024-09-03 01:59:56 +0530

    created basic ui of search page

---
+++ src/Pages/SearchPage.js
@@ -0,0 +1,13 @@
+import Header from "../Components/Layout/Header"
+import Search from "../Components/Search"
+
+const SearchPage = () => {
+  return (
+    <div>
+      <Header/>
+      <Search/>
+    </div>
+  )
+}
+
+export default SearchPage
